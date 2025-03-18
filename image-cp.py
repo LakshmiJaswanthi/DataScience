5ebbed16bcbcd986d02fd5dc605cd65046924df4
LakshmiJaswanthi <146002038+LakshmiJaswanthi@users.noreply.github.com>
2025-03-18 16:17:55 +0530

    Add files via upload

---
+++ image-cp.py
@@ -0,0 +1,21 @@
+import requests
+from PIL import Image
+from transformers import AutoProcessor,BlipForConditionalGeneration
+
+processor=AutoProcessor.from_pretrained("Salesforce/blip-image-captioning-base")
+model=BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-captioning-base")
+
+img_path="image.jpg"
+image=Image.open(img_path).convert('RGB')
+
+text="The image of"
+inputs=processor(images=image,text=text,return_tensors='pt')#tensor is the matrix or vector
+
+#print(inputs)
+
+output=model.generate(**inputs)
+
+#print(output)
+
+caption=processor.decode(output[0],skip_special_tokens=True)
+print(caption)
